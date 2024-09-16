98da594d64245608fe6f7f2b5a32c00fe6b019d9
Gulbahar57 <158152207+Gulbahar57@users.noreply.github.com>
2024-09-16 15:05:52 +0200

    add numberToString with unit testing

---
+++ numberToString.js
@@ -0,0 +1,6 @@
+/* return number into a string */
+
+export function numberToStrings(num){
+    let text = num.toString();
+    return text;
+}
---
+++ numberToString.test.js
@@ -0,0 +1,15 @@
+import { expect, test } from "vitest";
+import { numberToStrings, numberToString } from "./numberToString";
+
+test('devrait retourner un nombre sous forme de string', () => {
+    expect(numberToStrings(65)).toBe("65");
+    /* expect(numberToStrings([6, 5])).toBe("6, 5"); */ /* test failed, car la fonction ne prends pas en compte les tableaux */
+    expect(numberToStrings(-54)).toBe("-54");
+    expect(numberToStrings(100.54)).toBe("100.54");
+    /* expect(numberToStrings(987+789)).toBe("987+789");  *//* failed parce qu'il y a l'opérateur + entre */
+    expect(numberToStrings(0)).toBe("0");
+    expect(numberToStrings(15546794)).toBe("15546794");
+})
+
+/* améliorations : 
+faire en sorte à ce que ma fonction prenne en compte les tableaux et les cas mathématiques avec des additions ou autre. */
