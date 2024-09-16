947bf68074ee1c671bd9c8ea4280e2f199f4ae2c
Gulbahar57 <158152207+Gulbahar57@users.noreply.github.com>
2024-09-16 15:47:52 +0200

    add test

--- numberToString.test.js
+++ numberToString.test.js
@@ -9,7 +9,13 @@ test('devrait retourner un nombre sous forme de string', () => {
     /* expect(numberToStrings(987+789)).toBe("987+789"); failed parce qu'il y a l'opérateur + entre */
     expect(numberToStrings(0)).toBe("0");
     expect(numberToStrings(15546794)).toBe("15546794");
-    /* expect(numberToStrings(-0)).toBe("-0"); */
+    /* expect(numberToStrings(-0)).toBe("-0"); failed*/
+    expect(numberToStrings(-0.1)).toBe("-0.1");
+    expect(numberToStrings(Infinity)).toBe("Infinity");
+    expect(numberToStrings(-Infinity)).toBe("-Infinity");
+    expect(numberToStrings(NaN)).toBe("NaN");
+    expect(numberToStrings(1e20)).toBe("100000000000000000000");
+    expect(numberToStrings(1e-10)).toBe("1e-10");
 })
 
 /* améliorations : 
