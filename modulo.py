bf75d4f53ebbffed553afc5392fd9f0cf02a143b
Gulbahar57 <158152207+Gulbahar57@users.noreply.github.com>
2024-10-04 11:25:28 +0200

    adding fibonacci in python in my modulo.py

--- modulo.py
+++ modulo.py
@@ -5,3 +5,25 @@ def modulo(n):
         return "c'est un nombre impair"
 
 print(modulo(5))
+
+
+""" adding fibonacci in python """
+
+def fibonacci(n):
+    if n == 0:
+        return [0];
+    if n == 1:
+        return [0, 1];
+
+    a, b = 0, 1;
+    fibonacciArray = [a, b]
+
+    for i in range(2, n+1):
+        temp = a + b;
+        a = b;
+        b = temp
+        fibonacciArray.append(b);
+
+    return fibonacciArray;
+
+print(fibonacci(8));
